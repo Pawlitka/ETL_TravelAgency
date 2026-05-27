47e09f9ce33c4bdd922ab054c98feab29e5fdb8f
Pawlitka <pawel.kapusta2004@gmail.com>
2026-05-27 13:20:59 +0200

    add base from uni to main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test!");
-
-
+        File dataDir = new File("data");
+        TravelData travelData = new TravelData(dataDir);
+        String dateFormat = "yyyy-MM-dd";
+        for (String locale : Arrays.asList("pl_PL", "en_GB")) {
+            List<String> odlist = travelData.getOffersDescriptionsList(locale, dateFormat);
+            for (String od : odlist) System.out.println(od);
+        }
+        // --- część bazodanowa
+        String url = "asfa";/*<-- tu należy wpisać URL bazy danych */
+        Database db = new Database(url, travelData);
+        db.create();
+        db.showGui();
     }
 }
